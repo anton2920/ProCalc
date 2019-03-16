6ce07c9d872b3c64edfd8ae08ee4577443f285bb
anton <anton2920@gmail.com>
2019-03-16 10:36:44 +0300

    Revert "Anton: deleted project"
    This reverts commit 19431998df21151fb9fda64241d3c5e6b5c2cd3a.

---
+++ src/cmake-build-debug/CMakeFiles/src.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/anton/practice3/src/libs/polynom.lib.cpp" "/home/anton/practice3/src/cmake-build-debug/CMakeFiles/src.dir/libs/polynom.lib.cpp.o"
+  "/home/anton/practice3/src/libs/polynom.main.cpp" "/home/anton/practice3/src/cmake-build-debug/CMakeFiles/src.dir/libs/polynom.main.cpp.o"
+  "/home/anton/practice3/src/main.cpp" "/home/anton/practice3/src/cmake-build-debug/CMakeFiles/src.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
