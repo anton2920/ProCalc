a658fff062093971c71a051b637d48dd61afda1b
anton2920 <anton2920@gmail.com>
2019-03-16 20:51:34 +0300

    Anton: small changes

--- src/headers/Combinatorics.h
+++ src/headers/Combinatorics.h
@@ -17,6 +17,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with SetsCalc. If not, see <https://www.gnu.org/licenses/>.
 */
+
 #ifndef Combinatorics_h
 #define Combinatorics_h
 
