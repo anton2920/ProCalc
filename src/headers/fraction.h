ff619323933bc79e78287e0ab368e03455c47087
Andrew <kkk@yandex.ru>
2019-03-16 11:27:07 +0300

    Andrey: header

---
+++ src/headers/fraction.h
@@ -0,0 +1,23 @@
+#ifndef SOURCE_H
+#define SOURCE_H
+#include <iostream>
+struct Number {
+	int base;
+	int chis;
+	int znam;
+};
+int menu_calk_drob(void);
+void sravn(); //функция сравнения
+void read(Number *numb);//чтение
+int nod(struct Number, struct Number);//НОД
+int nok(int, int, int);//НОК
+void add(Number *numb1, Number *numb2);//сложение
+void vich(Number *numb1, Number *numb2);//вычитание
+void del(Number *numb1, Number *numb2);//деление
+void ymn(Number *numb1, Number *numb2);//умножение
+int videl(Number *numb);//выделение целой части и сокращение
+void per(Number *numb);//смешанная дробь
+void prev(Number *numb1, Number *numb2);//общий знаменатель
+void write(Number *numb);//вывод
+void base10(Number *numb);//десятичная дробь
+#endif
